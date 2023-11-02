501f4b8072a2911350378e130e7be2e88ee125c0
YuvrajBaserGkmit <yuvrajbaser@gkmit.co>
2023-11-02 18:41:31 +0530

    feat: add uuid extension and roles table

---
+++ models/role.js
@@ -0,0 +1,32 @@
+'use strict';
+const { Model } = require('sequelize');
+const rolePermission = require('./role-permission');
+module.exports = (sequelize, DataTypes) => {
+  class Role extends Model {
+    static associate(models) {
+      this.belongsToMany(models.Permission, {
+        through: 'RolePermission',
+        foreignKey: 'role_id',
+      });
+    }
+  }
+  Role.init(
+    {
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+    },
+    {
+      sequelize,
+      modelName: 'Role',
+      tableName: 'roles',
+      paranoid: true,
+    },
+  );
+  return Role;
+};
