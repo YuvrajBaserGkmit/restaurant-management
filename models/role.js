8ea6d66f418a6b75debfafe1900cf1278387548d
YuvrajBaserGkmit <yuvrajbaser@gkmit.co>
2023-11-03 11:24:41 +0530

    feat: add unique constraint for role title column

--- models/role.js
+++ models/role.js
@@ -15,6 +15,7 @@ module.exports = (sequelize, DataTypes) => {
       title: {
         type: DataTypes.STRING,
         allowNull: false,
+        unique: true,
       },
       description: {
         type: DataTypes.STRING,
