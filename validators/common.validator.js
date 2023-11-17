7fe486733ef26c8ca27ebc985fcdcdcdd711ac6f
YuvrajBaserGkmit <yuvrajbaser@gkmit.co>
2023-11-17 11:25:42 +0530

    feat: add role schema validators

---
+++ validators/common.validator.js
@@ -0,0 +1,26 @@
+const Joi = require('joi');
+const { validateRequest } = require('../helpers/validate.helper');
+
+const limitPageSchema = async (req, res, next) => {
+  const schema = Joi.object({
+    page: Joi.number().positive().allow(0),
+    limit: Joi.number().positive().min(1),
+  });
+  validateRequest(req, res, next, schema, 'query');
+};
+
+const idSchema = async (req, res, next) => {
+  const schema = Joi.object({
+    id: Joi.string()
+      .guid({
+        version: ['uuidv4'],
+      })
+      .required(),
+  });
+  validateRequest(req, res, next, schema, 'params');
+};
+
+module.exports = {
+  limitPageSchema,
+  idSchema,
+};
---
+++ validators/role.validator.js
@@ -0,0 +1,22 @@
+const Joi = require('joi');
+const { validateRequest } = require('../helpers/validate.helper');
+
+const createRoleSchema = (req, res, next) => {
+  const schema = Joi.object({
+    title: Joi.string().required().label('Title'),
+    description: Joi.string().label('Description'),
+  });
+  validateRequest(req, res, next, schema, 'body');
+};
+
+const deleteRoleSchema = (req, res, next) => {
+  const schema = Joi.object({
+    permanentDelete: Joi.boolean().label('Permanent Delete'),
+  });
+  validateRequest(req, res, next, schema, 'body');
+};
+
+module.exports = {
+  createRoleSchema,
+  deleteRoleSchema,
+};
