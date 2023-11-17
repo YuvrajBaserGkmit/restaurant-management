1a9119c354d24ba2d6fd236b35d6b309c833e29a
YuvrajBaserGkmit <yuvrajbaser@gkmit.co>
2023-11-17 18:08:29 +0530

    feat: add permission apis validator

---
+++ validators/permission.validator.js
@@ -0,0 +1,22 @@
+const Joi = require('joi');
+const { validateRequest } = require('../helpers/validate.helper');
+
+const createPermissionSchema = (req, res, next) => {
+  const schema = Joi.object({
+    title: Joi.string().required().label('Title'),
+    description: Joi.string().label('Description'),
+  });
+  validateRequest(req, res, next, schema, 'body');
+};
+
+const deletePermissionSchema = (req, res, next) => {
+  const schema = Joi.object({
+    permanentDelete: Joi.boolean().label('Permanent Delete'),
+  });
+  validateRequest(req, res, next, schema, 'query');
+};
+
+module.exports = {
+  createPermissionSchema,
+  deletePermissionSchema,
+};
