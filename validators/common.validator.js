135dc42b5ba96169dd87e9d75df5482d6ea908e6
YuvrajBaserGkmit <yuvrajbaser@gkmit.co>
2023-11-21 10:43:50 +0530

    feat: add common deleteSchema
    Signed-off-by: YuvrajBaserGkmit <yuvrajbaser@gkmit.co>

--- validators/common.validator.js
+++ validators/common.validator.js
@@ -3,8 +3,8 @@ const { validateRequest } = require('../helpers/validate.helper');
 
 const limitPageSchema = async (req, res, next) => {
   const schema = Joi.object({
-    page: Joi.number().positive().default(1),
-    limit: Joi.number().positive().min(1).default(10),
+    page: Joi.number().positive().max(100).default(1),
+    limit: Joi.number().positive().min(1).max(100).default(10),
   });
   validateRequest(req, res, next, schema, 'query');
 };
@@ -20,7 +20,15 @@ const idSchema = async (req, res, next) => {
   validateRequest(req, res, next, schema, 'params');
 };
 
+const deleteSchema = (req, res, next) => {
+  const schema = Joi.object({
+    permanentDelete: Joi.boolean().label('Permanent Delete'),
+  });
+  validateRequest(req, res, next, schema, 'query');
+};
+
 module.exports = {
   limitPageSchema,
   idSchema,
+  deleteSchema,
 };
