eff552e163929d469940cbaae564e478e22ab6a1
YuvrajBaserGkmit <yuvrajbaser@gkmit.co>
2023-11-21 10:58:47 +0530

    feat: add user validator

---
+++ validators/user.validator.js
@@ -0,0 +1,34 @@
+const Joi = require('joi');
+const { validateRequest } = require('../helpers/validate.helper');
+const passwordComplexity = require('joi-password-complexity');
+
+const complexityOptions = {
+  min: 8,
+  max: 16,
+  lowercase: 1,
+  uppercase: 1,
+};
+
+const createUserSchema = (req, res, next) => {
+  const schema = Joi.object({
+    firstName: Joi.string().min(3).max(30).required().label('First Name'),
+    lastName: Joi.string().min(3).max(30).required().label('Last Name'),
+    email: Joi.string().email().lowercase().required().label('Email'),
+    phoneNumber: Joi.string()
+      .length(10)
+      .pattern(/^[0-9]+$/)
+      .required()
+      .label('Phone Number'),
+    password: passwordComplexity(complexityOptions)
+      .required()
+      .label('Password'),
+    role: Joi.string().required().label('Role'),
+  });
+  validateRequest(req, res, next, schema, 'body');
+};
+
+module.exports = {
+  createUserSchema,
+  loginUserSchema,
+  refreshTokenSchema,
+};
