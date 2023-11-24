cd2c9e992b98aa210f86564abeeab37d5293d912
YuvrajBaserGkmit <yuvrajbaser@gkmit.co>
2023-11-24 13:00:26 +0530

    test: add restaurant test cases

--- tests/restaurant.test.js
+++ tests/restaurant.test.js
@@ -1,14 +1,8 @@
 const request = require('supertest');
-const { faker } = require('@faker-js/faker');
+const { faker, fa } = require('@faker-js/faker');
 const { sequelize } = require('../models');
 
 const app = require('../app');
-
-let payload = {
-  name: faker.company.companyName(),
-  address: faker.address.streetAddress(),
-  phoneNumber: faker.phone.phoneNumber(),
-};
 
 let adminAccessToken;
 
@@ -20,13 +14,65 @@ beforeAll(async () => {
   });
 
   if (admin) {
-    // Login and save the admin accessToken
     const resp = await request(app)
       .post('/api/auth/login')
       .send({ email: 'admin@gmail.com', password: 'Admin@1234' });
     adminAccessToken =
       resp.statusCode === 200 ? resp.body.data.accessToken : null;
   }
+});
+
+describe('TEST POST api/restaurants API', () => {
+  let payload;
+  beforeAll(async () => {
+    payload = {
+      name: faker.company.name(),
+      address: {
+        street: '100 feet road, Shobhagpura',
+        city: 'Udaipur',
+        state: 'Rajasthan',
+        pinCode: 458001,
+      },
+    };
+  });
+
+  it('should create a restaurant', async () => {
+    const res = await request(app)
+      .post('/api/restaurants')
+      .set('Authorization', `Bearer ${adminAccessToken}`)
+      .send(payload);
+
+    expect(res.body.message).toEqual('Success');
+    expect(res.statusCode).toBe(201);
+  });
+
+  it('should create a restaurant', async () => {
+    const res = await request(app)
+      .post('/api/restaurants')
+      .set('Authorization', `Bearer ${adminAccessToken}`)
+      .send(payload);
+
+    expect(res.body.message).toEqual('Restaurant already exists');
+    expect(res.statusCode).toBe(409);
+  });
+
+  it('should not allow invalid request', async () => {
+    const res = await request(app)
+      .post('/api/restaurants')
+      .set('Authorization', `Bearer ${adminAccessToken}`)
+      .send({
+        names: faker.company.name(),
+        addresses: faker.address.streetAddress(),
+      });
+    expect(res.body.message).toEqual('Name is required');
+    expect(res.statusCode).toBe(400);
+  });
+
+  it('should not allow access to unauthorized user', async () => {
+    const res = await request(app).post('/api/restaurants').send(payload);
+    expect(res.body.message).toEqual('Access denied');
+    expect(res.statusCode).toBe(401);
+  });
 });
 
 describe('TEST GET api/restaurants API', () => {
@@ -64,40 +110,20 @@ describe('TEST GET api/restaurants API', () => {
   });
 });
 
-describe('TEST POST api/restaurants API', () => {
-  it('should create a restaurant', async () => {
-    const res = await request(app)
-      .post('/api/restaurants')
-      .set('Authorization', `Bearer ${adminAccessToken}`)
-      .send(payload);
-
-    expect(res.body.message).toEqual('Success');
-    expect(res.statusCode).toBe(201);
-  });
-
-  it('should not allow invalid request', async () => {
-    const res = await request(app)
-      .post('/api/restaurants')
-      .set('Authorization', `Bearer ${adminAccessToken}`)
-      .send({
-        names: faker.company.companyName(),
-        addresses: faker.address.streetAddress(),
-      });
-    expect(res.body.message).toEqual('Name is required');
-    expect(res.statusCode).toBe(400);
-  });
-
-  it('should not allow access to unauthorized user', async () => {
-    const res = await request(app).post('/api/restaurants').send(payload);
-    expect(res.body.message).toEqual('Access denied');
-    expect(res.statusCode).toBe(401);
-  });
-});
-
 describe('TEST GET api/restaurants/:restaurantId API', () => {
   let restaurantId;
+  let payload;
 
   beforeAll(async () => {
+    payload = {
+      name: faker.company.name(),
+      address: {
+        street: '100 feet road, Shobhagpura',
+        city: 'Udaipur',
+        state: 'Rajasthan',
+        pinCode: 458001,
+      },
+    };
     const resp = await request(app)
       .post('/api/restaurants')
       .set('Authorization', `Bearer ${adminAccessToken}`)
@@ -145,8 +171,18 @@ describe('TEST GET api/restaurants/:restaurantId API', () => {
 
 describe('TEST PUT api/restaurants/:restaurantId API', () => {
   let restaurantId;
+  let payload;
 
   beforeAll(async () => {
+    payload = {
+      name: faker.company.name(),
+      address: {
+        street: '100 feet road, Shobhagpura',
+        city: 'Udaipur',
+        state: 'Rajasthan',
+        pinCode: 458001,
+      },
+    };
     const resp = await request(app)
       .post('/api/restaurants')
       .set('Authorization', `Bearer ${adminAccessToken}`)
@@ -156,9 +192,6 @@ describe('TEST PUT api/restaurants/:restaurantId API', () => {
   });
 
   it('should update restaurant by id', async () => {
-    payload.name = faker.company.companyName();
-    payload.address = faker.address.streetAddress();
-
     const res = await request(app)
       .put(`/api/restaurants/${restaurantId}`)
       .set('Authorization', `Bearer ${adminAccessToken}`)
@@ -192,15 +225,25 @@ describe('TEST PUT api/restaurants/:restaurantId API', () => {
       .put(`/api/restaurants/${restaurantId}`)
       .set('Authorization', `Bearer ${adminAccessToken}`)
       .send(payload);
-    expect(res.body.message).toEqual('restaurant not exists');
+    expect(res.body.message).toEqual('Restaurant not exists');
     expect(res.statusCode).toEqual(404);
   });
 });
 
 describe('TEST DELETE api/restaurants/:restaurantId API', () => {
   let restaurantId;
+  let payload;
 
   beforeAll(async () => {
+    payload = {
+      name: faker.company.name(),
+      address: {
+        street: '100 feet road, Shobhagpura',
+        city: 'Udaipur',
+        state: 'Rajasthan',
+        pinCode: 458001,
+      },
+    };
     const resp = await request(app)
       .post('/api/restaurants')
       .set('Authorization', `Bearer ${adminAccessToken}`)
@@ -208,7 +251,6 @@ describe('TEST DELETE api/restaurants/:restaurantId API', () => {
 
     restaurantId = resp.body.data.id;
   });
-
   it('should soft delete restaurant by id', async () => {
     const res = await request(app)
       .delete(`/api/restaurants/${restaurantId}`)
